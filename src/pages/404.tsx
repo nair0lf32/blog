ad10297c66d18321bcf59a5dbcf3c5e23fc66f27
nair0lf32 <florianedem@gmail.com>
2023-08-15 02:38:47 +0100

    upadte 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,34 +1,15 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import {
+  pageStyles, headingStyles, codeStyles,
+  paragraphStyles } from "../components/styles"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
+        Yup, you are lost...
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
