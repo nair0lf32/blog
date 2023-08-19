3ce8b4cf7ddcfd22946da2dbb90bdde4c8f80523
nair0lf32 <florianedem@gmail.com>
2023-08-19 23:36:28 +0100

    add new post template

---
+++ src/templates/post.tsx
@@ -0,0 +1,33 @@
+import { MDXProvider } from "@mdx-js/react"
+import { graphql, Link } from "gatsby"
+import React from "react"
+import Layout from "../components/layout"
+
+const shortcodes = { Link }
+
+const PostTemplate = ({ data, children }: any) => {
+    return(
+        <Layout>
+            <h1>{data.mdx.frontmatter.title}</h1>
+            <MDXProvider components={shortcodes}>
+            {children}
+            </MDXProvider>
+        </Layout>
+    )
+    }
+
+
+export const postQuery = graphql`
+    query($id: String) {
+        mdx(id: { eq: $id }) {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+            body
+        }
+        
+    } 
+    
+`
+export default PostTemplate
