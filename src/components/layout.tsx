75636c67ea3f4784e19d8ae89e24103ebb1caa97
nair0lf32 <florianedem@gmail.com>
2023-08-19 23:36:51 +0100

    add new layout file

---
+++ src/components/layout.tsx
@@ -0,0 +1,36 @@
+import { graphql } from "gatsby";
+import React from "react";
+
+//Styles
+export const pageStyle = {
+    margin: `3rem auto`, maxWidth: 650,
+    padding: `0 1rem`, fontFamily: `-apple-system, Roboto, sans-serif, serif`,
+    backgroundColor: `#f5f5f5`, color: `#232129`,
+}
+
+export const headingStyle = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+
+export const paragraphStyle = {
+  marginBottom: 48,
+}
+
+export const codeStyle = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+/* Main layout */
+export default function Layout({ data, children }: any) {
+    return (
+        <main style={pageStyle}>
+            { children }
+        </main>
+  )
+}
