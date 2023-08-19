da69ea306d2b1ab0f076fa36913f7971e361806a
nair0lf32 <florianedem@gmail.com>
2023-08-19 23:37:52 +0100

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,11 @@
-import * as React from "react"
-import { HeadFC, PageProps, Link } from "gatsby"
-import {
-  pageStyles, headingStyles, headingAccentStyles,
-  paragraphStyles } from "../components/styles"
+import React from "react";
+import Layout from "../components/layout";
+import { Link } from "gatsby";
 
-
-
-const IndexPage: React.FC<PageProps> = () => {
+export default function Home() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Blog &#x1f4bb;
-        <br />
-        <span style={headingAccentStyles}>— Welcome to my muthafuckin' blog </span>
-      </h1>
-      <p style={paragraphStyles}>
-        This is my technical blog. <br/>
-        Here is a <Link to="/posts/">list of all posts</Link>
-      </p>
-    </main>
+    <Layout>
+      <Link to={'/posts'}>All posts</Link>
+    </Layout>
   )
 }
-
-export default IndexPage
-
-export const Head: HeadFC = () => <title>Home Page</title>
