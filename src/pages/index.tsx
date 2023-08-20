e759459473888fbaccba80ecf43f8c4a060530ab
nair0lf32 <florianedem@gmail.com>
2023-08-20 13:10:28 +0100

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import Layout from "../components/layout";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
+
+
+export const allPostsQuery = graphql`
+query {
+  site {
+    siteMetadata {
+      title
+      description
+    }
+  }
+}
+`
+
 
 export default function Home() {
   return (
