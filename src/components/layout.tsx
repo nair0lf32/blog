f82f2062abeec6b9b6ee2f8a3b2c6084bbee31aa
nair0lf32 <florianedem@gmail.com>
2023-12-29 23:09:16 +0100

    update layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,32 +2,24 @@ import React from "react";
 
 /* styles */
 export const pageStyle = {
-  margin: `1rem auto`,
+  margin: "1rem auto",
   maxWidth: 800,
-  padding: `0 1rem`,
-  fontFamily: `-apple-system, Roboto, sans-serif, serif`,
-  backgroundColor: `#f5f5f5`,
-  color: `#232129`,
+  padding: "2rem 1rem",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor: "#f2f2f2",
+  color: "#333",
 }
 
 export const headingStyle = {
   marginTop: 0,
   marginBottom: 32,
   fontSize: "2rem",
+  color: "#004080",
 }
 
 export const paragraphStyle = {
   marginBottom: 16,
   fontSize: "1rem",
-}
-
-export const codeStyle = {
-  color: "#fff",
-  padding: "0.5rem",
-  backgroundColor: "#1e1e1e",
-  fontSize: "1rem",
-  borderRadius: 4,
-  overflowX: "auto",
 }
 
 /* Main layout */
