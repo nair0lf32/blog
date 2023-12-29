d762c3ca4255cab1ff122301b7cc74e9828075e6
nair0lf32 <florianedem@gmail.com>
2023-12-29 15:31:19 +0100

    update layout styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,34 +1,40 @@
 import React from "react";
 
-//Styles
+/* styles */
 export const pageStyle = {
-    margin: `1.5rem auto`, maxWidth: 850,
-    padding: `0 0.5rem`, fontFamily: `-apple-system, Roboto, sans-serif, serif`,
-    backgroundColor: `#f5f5f5`, color: `#232129`,
+  margin: `1rem auto`,
+  maxWidth: 800,
+  padding: `0 1rem`,
+  fontFamily: `-apple-system, Roboto, sans-serif, serif`,
+  backgroundColor: `#f5f5f5`,
+  color: `#232129`,
 }
 
 export const headingStyle = {
   marginTop: 0,
-  marginBottom: 64,
+  marginBottom: 32,
+  fontSize: "2rem",
 }
 
 export const paragraphStyle = {
-  marginBottom: 20,
+  marginBottom: 16,
+  fontSize: "1rem",
 }
 
 export const codeStyle = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
+  color: "#fff",
+  padding: "0.5rem",
+  backgroundColor: "#1e1e1e",
+  fontSize: "1rem",
   borderRadius: 4,
+  overflowX: "auto",
 }
 
 /* Main layout */
-export default function Layout({ data, children }: any) {
-    return (
-        <body style={pageStyle}>
-            { children }
-        </body>
+export default function Layout({ children }: any) {
+  return (
+    <div style={pageStyle}>
+      {children}
+    </div>
   )
 }
