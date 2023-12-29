c96549781375091f103ff43ff5b2c82296834e1f
nair0lf32 <florianedem@gmail.com>
2023-12-29 23:09:34 +0100

    update post template

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,32 +1,31 @@
-import { MDXProvider } from "@mdx-js/react"
-import { graphql, Link } from "gatsby"
-import React from "react"
-import Layout, { headingStyle } from "../components/layout"
+import { MDXProvider } from "@mdx-js/react";
+import { graphql, Link } from "gatsby";
+import React from "react";
+import Layout, { headingStyle} from "../components/layout";
 
-const shortcodes = { Link }
+const shortcodes = { Link };
 
 const PostTemplate = ({ data, children }: any) => {
-    return(
-        <Layout>
-            <h1 style={headingStyle}>{data.mdx.frontmatter.title}</h1>
-            <MDXProvider components={shortcodes}>
-            {children}
-            </MDXProvider>
-        </Layout>
-    )
-    }
+  return (
+    <Layout>
+      <h1 style={headingStyle}>{data.mdx.frontmatter.title}</h1>
+      <MDXProvider components={{ ...shortcodes,}}>
+        {children}
+      </MDXProvider>
+    </Layout>
+  );
+};
 
 export const postQuery = graphql`
-    query($id: String) {
-        mdx(id: { eq: $id }) {
-            frontmatter {
-                title
-                date(formatString: "MMMM D, YYYY")
-            }
-            body
-        }
-        
-    } 
-    
-`
-export default PostTemplate
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default PostTemplate;
