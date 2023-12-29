4368c7c7852dac3d30719303beeb7ce3117e1833
nair0lf32 <florianedem@gmail.com>
2023-12-29 15:32:58 +0100

    update posts

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -2,41 +2,40 @@ import { Link, graphql } from "gatsby";
 import React from "react";
 import Layout, { headingStyle } from "../components/layout";
 
-
 export const allPostsQuery = graphql`
-query {
-  allMdx(sort: {frontmatter: {date: DESC}}) {
-    nodes {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
-        slug
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
+          slug
+        }
+        excerpt
       }
-      excerpt
     }
   }
-}
-`
+`;
 
 const PostList = ({ data, children }: any) => {
-    return(
-      <Layout>
-        <h1 style={headingStyle}>All Blog Posts</h1>
-            <ul>
-                {data.allMdx.nodes.map((node: any) => (
-                    <li key={node.frontmatter.slug}>
-                    <div>
-			<Link to={`/posts${node.frontmatter.slug}`}>
-                        <h2>{node.frontmatter.title}</h2>
-                        </Link>
-                        <p>{node.frontmatter.date}</p>
-                        <p>{node.excerpt}</p>
-                    </div>
-                    </li> 
-                ))}
-            </ul>
-        </Layout>
-    )
-    }
+  return (
+    <Layout>
+      <h1 style={headingStyle}>All Blog Posts</h1>
+      <ul>
+        {data.allMdx.nodes.map((node: any) => (
+          <li key={node.frontmatter.slug}>
+            <div>
+              <Link to={`/posts${node.frontmatter.slug}`}>
+                <h2>{node.frontmatter.title}</h2>
+              </Link>
+              <p>{node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
 export default PostList;
